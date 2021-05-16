0281157d22ba763c25a881c540d4cac66ee5b09f
Capprin Bass <capprin@gmail.com>
2021-05-15 21:48:44 -0700

    Broke out static,frictional force

--- project/actuatorForce.m
+++ project/actuatorForce.m
@@ -4,7 +4,8 @@
     % P: pressure
     % vel: velocity
     % a: actuator struct
-function F = actuatorForce(eps, P, vel, a)
+function [F, F_s, F_f] = actuatorForce(eps, P, vel, a)
+    % sanitize
     if eps < 0
         warning('Model in extension causes residual imaginary components')
         [~, id] = lastwarn;
@@ -16,12 +17,13 @@ function F = actuatorForce(eps, P, vel, a)
     c2 = 1/sin(a.a0)^2;
     F_s = pi*a.r0^2.*P.*(c1.*(1-eps).^2-c2);
     
-    % coeff. of friction (using approx. contact area)
+    % frictional force
     S = 2.*pi.*a.r0.*a.l0.*sin(a.a0)./((1-eps).*sqrt(1-cos(a.a0).^2.*(1-eps).^2));
     mu = a.fk + (a.fs - a.fk)*exp(vel/a.vf);
+    F_f = mu.*S.*P.*sign(vel);
     
     % dynamic force (negative for contraction)
-    F = -(F_s - mu.*S.*P.*sign(vel));
+    F = -(F_s - F_f);
     if eps < 0
         F = real(F);
     end
