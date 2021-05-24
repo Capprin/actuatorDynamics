d0b939c0b125d8ba5899fa216f7dd1e4246abac2
Farhan <nmfarhan@gmail.com>
2021-05-24 14:27:27 -0700

    Updated position control

--- project/actuatorControl.m
+++ project/actuatorControl.m
@@ -1,11 +1,30 @@
 % defines pressure control signal dependent on state
 function P = actuatorControl(t,x,a)
-    bar = @(psi) 0.0689476 * psi;
+    
     Kp = 30;
     Kd = 5;
     
-    P_des = bar(100) * abs(sin(t*pi/2.5)); %periodic pressure; ea. 2.5sec
-    dP_des = bar(100) * (pi/2.5) * abs(-cos(t*pi/2.5));
+    bar = @(psi) 0.0689476 * psi;
     
-    P = Kp*(P_des - x(1)) + Kd*(dP_des - x(2));
+%     P_des = bar(100) * (1+sin(t*pi/2.5)); %periodic pressure; ea. 2sec
+    
+%     dP_des = bar(100) * (pi/2.5) * (cos(t*pi/2.5));
+    
+    x_des = a.x_des(t);
+    
+    dx_des = a.dx_des(t);
+    
+    P = 100.*Kp.*(-x_des + x(1)) + 100.*Kd.*(-dx_des + x(2));
+    
+    if P<0
+        P = 0;
+    end
+
+%     P = bar(-900);
+    
+%     if t<3
+%         P = bar(5000);
+%     else
+%         P = bar(0);
+%     end
 end
--- project/mainActuatorSim.m
+++ project/mainActuatorSim.m
@@ -21,8 +21,8 @@ a.a0 = 2*pi/360*20;
 a.x0 = [0; 0];
 a.m = 1;
 % parallel spring parameters
-a.k = 1;
-a.d = 0;
+a.k = 50;
+a.d = -10;
 % sheath friction
 a.do_f = false;
 a.fs = 0.015;
@@ -31,6 +31,11 @@ a.vf = 0.15; %trans. vel for s-k friction
 % behavior
 a.s = @(t,x) actuatorSensing(t,x,a); %len/vel as a fn of state
 a.c = @(t,x) actuatorControl(t,x,a); %pressure as a fn of state
+% desired behavior
+per = 2;
+freq = 1/per;
+a.x_des = @(t) (3/4)+(cos(t.*pi./freq))./4;
+a.dx_des = @(t) -sin(t.*pi./freq)./4.*pi./freq;
 
 % load parameters
 l.m = 1;
@@ -38,20 +43,24 @@ l.x0 = [1; 0]; %defines length
 l.f = @(t,x) 0;
 
 % simulation
-t_max = 5;
+t_max = 10;
 [t_vec, x_vec] = actuatorSim(a,l,t_max);
 
 % plots
 figure(1);
+set(gcf, 'Position',  [100, 100, 1000, 800])
 % position
 subplot(3,1,1);
 plot(t_vec, x_vec(:,1));
 xlabel('Time (s)');
 ylabel('Position (m)');
 title('McKibben End Position');
+% legend('End effector position', 'Desired position');
 % EE force
 subplot(3,1,2);
 eps_vec = (a.l0-x_vec(:,1))/a.l0;
+% pressure
+P = arrayfun(@(t,x1, x2) actuatorControl(t,[x1 x2],a), t_vec, x_vec(:,1), x_vec(:,2));
 P_vec = actuatorControl(t_vec, x_vec, a);
 [F_vec, F_s, F_f, F_sp] = actuatorForce(eps_vec, P_vec, x_vec(:,2), a);
 plot(t_vec, F_vec);
@@ -64,6 +73,13 @@ xlabel('Time (s)');
 ylabel('Force (N)');
 title('McKibben Force Components');
 legend('Static Force', 'Frictional Force', 'Spring Force');
+% pressure plot
+figure(3);
+set(gcf, 'Position',  [100, 100, 1000, 800])
+plot(t_vec,P);
+title('McKibben Internal Presssure');
+xlabel('Time (s)');
+ylabel('Pressure (bar)');
 
 % animation
 figure(2);
