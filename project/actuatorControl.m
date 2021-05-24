4b6312a0b0bc3594d58fb27a92f4f2a538f56342
Capprin Bass <capprin@gmail.com>
2021-05-24 10:18:28 -0700

    Updated repo with Alp's controller updates

--- project/actuatorControl.m
+++ project/actuatorControl.m
@@ -1,6 +1,11 @@
 % defines pressure control signal dependent on state
 function P = actuatorControl(t,x,a)
     bar = @(psi) 0.0689476 * psi;
-    P = bar(100) * abs(sin(t*pi/2.5)); %periodic pressure; ea. 2sec
-    %P = bar(45);
+    Kp = 30;
+    Kd = 5;
+    
+    P_des = bar(100) * abs(sin(t*pi/2.5)); %periodic pressure; ea. 2.5sec
+    dP_des = bar(100) * (pi/2.5) * abs(-cos(t*pi/2.5));
+    
+    P = Kp*(P_des - x(1)) + Kd*(dP_des - x(2));
 end
