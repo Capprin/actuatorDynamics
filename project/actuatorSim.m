a360c1e04f6ae8edfcacbfdddc86b17806640c6a
Capprin Bass <capprin@gmail.com>
2021-05-31 16:53:29 -0700

    Updated simulation to include pressure in the SS

--- project/actuatorSim.m
+++ project/actuatorSim.m
@@ -2,19 +2,23 @@
 % hybrid sim allows for contact events
 function [t_vec, x_vec] = actuatorSim(a, l, t_max)
     % set up ode45
-    x0 = a.x0 + l.x0;
+    x0 = a.x0 + [l.x0; 0];
     [t_vec, x_vec] = ode45(@(t,x) dynamics(t,x,a,l), [0 t_max], x0);
 end
 
-% state: a_pos, a_vel, l_pos, l_vel
+% simulates McKibben dynamics under control
+% state: [position, velocity, pressure]
 function dx = dynamics(t,x,a,l)
     % velocity
     vel = x(2);
-    % compute force
+    % compute force using pressure
     eps = (a.l0-x(1))/a.l0;
-    P = a.c(t,x);
-    F = actuatorForce(eps,P,vel,a) + l.f(t,x);
+    F = actuatorForce(eps,x(3),vel,a) + l.f(t,x);
+    % update pressure with control
+    p_des = a.c(t,x);
+    dP = pressureControl(a, p_des, x(3));
     % accelerate both actuator and load at once
     accel = F/l.m;
-    dx = [vel; accel];
+    dx = [vel; accel; dP];
 end
+
--- project/mainActuatorSim.m
+++ project/mainActuatorSim.m
@@ -4,18 +4,21 @@
 a.l0 = 1;
 a.r0 = 0.02;
 a.a0 = 2*pi/360*20;
-a.x0 = [0; 0];
+a.x0 = [0; 0; 0];
 % parallel spring parameters
 a.k = 200;
 a.d = -30;
+% limits
+a.p_max = 60;
+a.p_min = 0;
+a.dp_max = 110;
+% desired behavior
+freq = 3/2; %cycles/second
+a.x_des = @(t) (7/8)+(cos(2*t.*pi.*freq))./8;
+a.dx_des = @(t) -sin(2*t.*pi.*freq)./8*2.*pi.*freq;
 % behavior
 a.s = @(t,x) actuatorSensing(t,x,a); %len/vel as a fn of state
 a.c = @(t,x) actuatorControl(t,x,a); %pressure as a fn of state
-% desired behavior
-per = 1;
-freq = 1/per;
-a.x_des = @(t) (7/8)+(cos(t.*pi./freq))./8;
-a.dx_des = @(t) -sin(t.*pi./freq)./8.*pi./freq;
 
 % load parameters
 l.m = 1;
@@ -23,44 +26,53 @@ l.x0 = [1; 0]; %defines length
 l.f = @(t,x) 0;
 
 % simulation
-t_max = 10;
+t_max = 5;
 [t_vec, x_vec] = actuatorSim(a,l,t_max);
 
 % plots
 figure(1);
 set(gcf, 'Position',  [100, 100, 1000, 800])
 % position
-subplot(3,1,1);
-plot(t_vec, x_vec(:,1));
+subplot(4,1,1);
+plot(t_vec, [x_vec(:,1) a.x_des(t_vec)]);
 xlabel('Time (s)');
 ylabel('Position (m)');
 title('McKibben End Position');
-% legend('End effector position', 'Desired position');
-% EE force
-subplot(3,1,2);
-eps_vec = (a.l0-x_vec(:,1))/a.l0;
+legend('End effector position', 'Desired position');
 % pressure
-P = arrayfun(@(t,x1, x2) actuatorControl(t,[x1 x2],a), t_vec, x_vec(:,1), x_vec(:,2));
-P = P*14.504;
-P_vec = actuatorControl(t_vec, x_vec, a);
+subplot(4,1,2);
+P_vec = x_vec(:,3);
+P_des = arrayfun(@(t,x,v) a.c(t, [x v]), t_vec, x_vec(:,1), x_vec(:,2));
+cla;
+hold on;
+plot(t_vec, 14.504*[P_vec P_des]); %adjusted bar -> psi
+plot(t_vec, zeros(size(t_vec)), '--');
+hold off;
+title('McKibben Internal Presssure');
+xlabel('Time (s)');
+ylabel('Pressure (psi)');
+legend('Internal Pressure', 'Desired Pressure');
+% EE force
+subplot(4,1,3);
+eps_vec = (a.l0-x_vec(:,1))/a.l0;
 [F_vec, F_s, F_sp] = actuatorForce(eps_vec, P_vec, x_vec(:,2), a);
 plot(t_vec, F_vec);
 xlabel('Time (s)');
 ylabel('Force (N)');
 title('McKibben Aggregate End Force');
-subplot(3,1,3);
+subplot(4,1,4);
 plot(t_vec, [-F_s, F_sp]);
 xlabel('Time (s)');
 ylabel('Force (N)');
 title('McKibben Force Components');
-legend('Static Force', 'Frictional Force', 'Spring Force');
-% pressure plot
+legend('Static Force', 'Spring Force');
+
 figure(2);
-set(gcf, 'Position',  [100, 100, 1000, 800])
-plot(t_vec,P);
-title('McKibben Internal Presssure');
+dP_vec = arrayfun(@(p_des, p_curr) pressureControl(a, p_des, p_curr), P_des, x_vec(:,3));
+plot(t_vec, 14.504*dP_vec);
+title('Differential Pressure');
 xlabel('Time (s)');
-ylabel('Pressure (psi)');
+ylabel('P/s (psi)');
 
 % animation
 figure(3);
